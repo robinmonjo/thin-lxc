e056004e03134d15d79aafdad28f1e7a9ddadcc8
Robin Monjo <robin.monjo@applidget.com>
2014-01-29 21:51:20 +0100

    do not hardcode version

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "Downloading sources"
 curl -sL https://github.com/robinmonjo/thin-lxc/archive/v$VERSION.tar.gz | tar -C /tmp -zxf -
 
 echo "Building ..."
-go build /tmp/thin-lxc-0.2/thin-lxc.go
+go build /tmp/thin-lxc-$VERSION/thin-lxc.go
 sudo mv thin-lxc /usr/local/bin
 
 echo "Cleaning up ..."
